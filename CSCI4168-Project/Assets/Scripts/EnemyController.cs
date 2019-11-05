8964fa25d35983eb6f49e6a7386743bc27d5bded
moobrock <moobrock1@gmail.com>
2019-11-05 13:17:42 -0400

    Added simple enemy controller and barn location
    Enemy moves towards barn location.

---
+++ CSCI4168-Project/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    private Vector3 destination;
+
+    private float moveDist = 1f;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        destination = GameManager.gameManager.barnAttackPosition.position;
+
+        SetDestination();
+    }
+
+    private void SetDestination()
+    {
+        SetDestination(destination);
+    }
+
+    private void SetDestination(Vector3 position)
+    {
+        destination = position;
+        navMeshAgent.SetDestination(position);
+    }
+
+    private void Update()
+    {
+        DevelopmentControls();
+    }
+
+    // manually control enemy for development
+    private void DevelopmentControls()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            navMeshAgent.Move(new Vector3(0, 0, moveDist));
+        }
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            navMeshAgent.Move(new Vector3(0, 0, -moveDist));
+        }
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            navMeshAgent.Move(new Vector3(-moveDist, 0, 0));
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            navMeshAgent.Move(new Vector3(moveDist, 0, 0));
+        }
+    }
+}
--- CSCI4168-Project/Assets/Scripts/GameManager.cs
+++ CSCI4168-Project/Assets/Scripts/GameManager.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Transform[] towerAttackPositions;
+    public Transform barnAttackPosition;
 
+    public static GameManager gameManager;
 
+    private void Awake()
+    {
+        if (gameManager != null)
+        {
+            Destroy(gameManager);
+        }
+
+        gameManager = this;
+    }
 }
