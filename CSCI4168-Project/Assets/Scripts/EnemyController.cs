c9ef718af2969a30aa2a503469d177a85342e564
moobrock <moobrock1@gmail.com>
2019-11-06 17:10:34 -0400

    Added basic tower attacking.
    Added ability for enemy to approach, attack, and ultimately destroy towers it runs into on its path. Also added prefab placeholders for towers, barn, and a basic enemy.

--- CSCI4168-Project/Assets/Scripts/EnemyController.cs
+++ CSCI4168-Project/Assets/Scripts/EnemyController.cs
@@ -10,17 +10,24 @@ public class EnemyController : MonoBehaviour
 
     private float moveDist = 1f;
 
+    private float baseDamage = 0.1f;        // base damage done every attackFreqency seconds
+    private float damageModifier = 0.05f;   // slight random modifier (+/-) to damage
+    private float attackFrequency = 1f;     // in seconds
+    private float attackRange = 1f;         // can only attack in this range
+
+    private TowerController attackTarget;
+
     private void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-
-        destination = GameManager.gameManager.barnAttackPosition.position;
 
         SetDestination();
     }
 
     private void SetDestination()
     {
+        destination = GameManager.gameManager.barnAttackPosition.position;
+
         SetDestination(destination);
     }
 
@@ -32,6 +39,9 @@ public class EnemyController : MonoBehaviour
 
     private void Update()
     {
+        // keep rotation
+        transform.rotation = Quaternion.identity;
+
         DevelopmentControls();
     }
 
@@ -57,5 +67,76 @@ public class EnemyController : MonoBehaviour
         {
             navMeshAgent.Move(new Vector3(moveDist, 0, 0));
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Tower" || other.tag == "Barn")
+        {
+            // get tower attack position
+            TowerController tower = other.GetComponent<TowerController>();
+
+            if (tower != null)
+            {
+                Transform attackPosition = tower.GetAttackPosition();
+
+                // TODO: use navmesh agent to check path distance (to make sure the tower isn't on another nearby path)
+                float pathDistance = TowerController.attackRadius;
+
+                // go to attack position if on the same path (navmesh agent path distance is short)
+                if (pathDistance <= TowerController.attackRadius * 2f)
+                {
+                    SetDestination(attackPosition.position);
+
+                    attackTarget = tower;
+
+                    StartCoroutine(ApproachTower());
+                }
+            }
+        }
+    }
+
+    private IEnumerator ApproachTower()
+    {
+        while (attackTarget != null)
+        {
+            float distance = (transform.position - attackTarget.GetAttackPosition().position).magnitude;
+
+            // in attack range of tower
+            // start attacking and exit coroutine
+            if (distance <= attackRange)
+            {
+                StartCoroutine(AttackTower());
+
+                yield break;
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private IEnumerator AttackTower()
+    {
+        while (attackTarget != null && attackTarget.GetHealth() > 0f)
+        {
+            float damage = baseDamage + Random.Range(-damageModifier, damageModifier);
+
+            bool success = attackTarget.Attack(damage);
+
+            Debug.Log("Attacking " + attackTarget.tag + " for " + damage + " damage. Tower health = " + attackTarget.GetHealth());
+
+            // enemy is or was dead
+            // stop attacking and exit coroutine
+            if (!success)
+            {
+                attackTarget = null;
+
+                yield break;
+            }
+
+            yield return new WaitForSecondsRealtime(attackFrequency);
+        }
+
+        SetDestination();
     }
 }
---
+++ CSCI4168-Project/Assets/Scripts/TowerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour
+{
+    private Transform attackPosition;
+    private GameObject towerModel;
+
+    public static float attackRadius = 5f;
+    private static float health = 1f;
+
+    void Start()
+    {
+        attackPosition = transform.GetChild(0);
+        towerModel = transform.GetChild(1).gameObject;
+    }
+    
+    public Transform GetAttackPosition() { return attackPosition; }
+
+    public float GetHealth() { return health; }
+
+    // true -> still alive (attack successful)
+    // false -> dead (attack unsuccessful)
+    public bool Attack(float damage)
+    {
+        if (health > 0f)
+        {
+            health = Mathf.Max(0f, health - damage);
+
+            if (health <= 0f)
+            {
+                DestroyTower();
+            }
+
+            return true;
+        }
+
+        DestroyTower();
+
+        return false;
+    }
+
+    private void DestroyTower()
+    {
+        towerModel.SetActive(false);
+    }
+}
