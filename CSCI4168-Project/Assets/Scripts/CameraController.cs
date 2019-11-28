f99f10d45ee5050ecf238eb05b53b97cffc63e8c
moobrock <moobrock1@gmail.com>
2019-11-27 22:15:05 -0400

    Lower icon drop and improved camera movement

--- CSCI4168-Project/Assets/Scripts/CameraController.cs
+++ CSCI4168-Project/Assets/Scripts/CameraController.cs
@@ -6,9 +6,9 @@ public class CameraController : MonoBehaviour
 {
     private RectTransform cameraBoundary;   // world-space rect transform boundary of the map
 
-    private float boundaryWidth = 40f;
+    private float boundaryWidth = 80f;
 
-    private float speed = 0.25f;
+    private float speed = 0.015f;
 
     private void Start()
     {
@@ -30,36 +30,17 @@ public class CameraController : MonoBehaviour
             mousePositionScreen.y >= 0 && 
             mousePositionScreen.y <= Screen.height)
         {
+            // distance of mouse position from edge of game screen
+            float diff = Mathf.Max(boundaryWidth - mousePositionScreen.x,
+                mousePositionScreen.x - (Screen.width - boundaryWidth),
+                boundaryWidth - mousePositionScreen.y,
+                mousePositionScreen.y - (Screen.height - boundaryWidth));
+
             // move camera if mouse position is near edge of the screen
-            if ((mousePositionScreen.x < boundaryWidth) ||                  // left
-                (mousePositionScreen.x > (Screen.width - boundaryWidth)) || // right
-                (mousePositionScreen.y < boundaryWidth) ||                  // down
-                (mousePositionScreen.y > (Screen.height - boundaryWidth)))  // up
+            if (diff > 0f)
             {
-                transform.position = Vector3.MoveTowards(transform.position, mousePositionWorld, speed);
-
-                // TODO: keep camera within bounds of cameraBoundary
-                /*
-                Vector3[] frustumCorners = new Vector3[4];
-                Camera.main.CalculateFrustumCorners(new Rect(0, 0, 1, 1), Camera.main.farClipPlane, Camera.MonoOrStereoscopicEye.Mono, frustumCorners);
-
-                for (int i = 0; i < 4; i++)
-                {
-                    var worldSpaceCorner = transform.TransformVector(frustumCorners[i]);
-                    Debug.DrawRay(transform.position, worldSpaceCorner, Color.blue);
-                }
-
-                Vector3 center = new Vector3(cameraBoundary.rect.center.x + cameraBoundary.transform.position.x, 0, cameraBoundary.rect.center.y + cameraBoundary.transform.position.z);
-                Vector3 min = new Vector3(cameraBoundary.rect.xMin, 0, cameraBoundary.rect.yMin);
-                Vector3 max = new Vector3(cameraBoundary.rect.xMax, 0, cameraBoundary.rect.yMax);
-
-                Debug.Log("xMin: " + min.x + " zMin: " + min.z + " xMax: " + max.x + " zMax: " + max.z);
-
-                Debug.DrawRay(center, min, Color.green);
-                Debug.DrawRay(center, max, Color.red);
-
-                Vector3 newPosition = transform.position;
-                */
+                transform.position = Vector3.MoveTowards(transform.position, mousePositionWorld, speed * diff);
+                Debug.Log(diff);
             }
         }
     }
