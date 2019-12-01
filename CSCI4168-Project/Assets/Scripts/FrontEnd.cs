9019da07eadc41750fae77eecc71217c344d2c7b
moobrock <moobrock1@gmail.com>
2019-12-01 10:44:33 -0400

    Moved tutorial to separate page and added some rules

---
+++ CSCI4168-Project/Assets/Scripts/FrontEnd.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontEnd : MonoBehaviour
+{
+    public GameObject mainPanel;
+    public GameObject rulesPanel;
+
+    public void ShowRulesPanel()
+    {
+        mainPanel?.SetActive(false);
+        rulesPanel?.SetActive(true);
+    }
+
+    public void ShowMainPanel()
+    {
+        mainPanel?.SetActive(true);
+        rulesPanel?.SetActive(false);
+    }
+}
